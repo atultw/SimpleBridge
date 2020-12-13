d39c7dc04a6c9dbf727ac16967a13b1a574e6e62
atultw <30200282+atultw@users.noreply.github.com>
2020-12-12 17:20:44 -0800

    1

--- src/main/java/io/github/atultw/gmsbridge/InternalSchematic.java
+++ src/main/java/io/github/atultw/gmsbridge/InternalSchematic.java
@@ -1,40 +1,32 @@
 package io.github.atultw.gmsbridge;
 
+import com.boydti.fawe.object.schematic.Schematic;
 import com.sk89q.worldedit.*;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
-import com.sk89q.worldedit.command.SchematicCommands;
 import com.sk89q.worldedit.data.DataException;
-import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
-import com.sk89q.worldedit.extent.clipboard.io.SchematicWriter;
-import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
-import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.regions.CuboidRegion;
-import com.sk89q.worldedit.schematic.MCEditSchematicFormat;
 import com.sk89q.worldedit.schematic.SchematicFormat;
-import org.bukkit.Location;
+import com.sk89q.worldedit.util.Location;
+import org.bukkit.World;
 
 import java.io.File;
 import java.io.IOException;
 
 public class InternalSchematic {
-    public void New(MapDef m) throws WorldEditException {
-        Location l1 = m.getC1l();
-        Location l2 = m.getC2l();
-        CuboidRegion region = new CuboidRegion(l1.getWorld(), l1, l2);
-        BlockArrayClipboard clipboard = new BlockArrayClipboard(region);
-
-        EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(region.getWorld(), -1);
-
-        ForwardExtentCopy forwardExtentCopy = new ForwardExtentCopy(editSession, region, clipboard, region.getMinimumPoint());
-        Operations.complete(forwardExtentCopy);
-
+    public void New(MapDef m, World w, Location l1, Location l2) throws IOException {
+        File file = new File(m.getArenaName()+ ".schem");
+        Vector bot = new Vector(l1.getBlockX(), l1.getBlockY(), l1.getBlockZ());
+        Vector top = new Vector(l2.getBlockX(), l2.getBlockY(), l2.getBlockZ());
+        CuboidRegion region = new CuboidRegion(new BukkitWorld(w), bot, top);
+        Schematic schem = new Schematic(region);
+        schem.save(file, ClipboardFormat.SCHEMATIC);
     }
-    public void Paste(MapDef m) throws IOException, WorldEditException, DataException {
-        Location l = m.getC1l();
-        File file = new File(m.getArenaName() + ".schem");
-        Vector v = new Vector(l.getBlockX(), l.getBlockY(), l.getBlockZ()); // loc is your location variable.
-        EditSession es = WorldEdit.getInstance().getEditSessionFactory().getEditSession(new BukkitWorld(l.getWorld()), WorldEdit.getInstance().getConfiguration().maxChangeLimit)
+
+    public void Load(MapDef m, World w, Location l1, Location l2) throws IOException, DataException, MaxChangedBlocksException {
+        File file = new File(m.getArenaName()+ ".schem");
+        Vector v = new Vector(l1.getBlockX(), l1.getBlockY(), l1.getBlockZ()); // loc is your location variable.
+        EditSession es = WorldEdit.getInstance().getEditSessionFactory().getEditSession(new BukkitWorld(w), WorldEdit.getInstance().getConfiguration().maxChangeLimit);
         SchematicFormat format = SchematicFormat.getFormat(file);
         CuboidClipboard cc = format.load(file);
         cc.paste(es, v, false);
